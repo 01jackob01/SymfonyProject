0991199a0953e05c38acd2fe876a194714c5ef9a
jmickiewicz <j.mickiewicz@madkom.pl>
2021-04-27 20:54:33 +0200

    Current Dante

--- src/Controller/CurrentDateController.php
+++ src/Controller/CurrentDateController.php
@@ -1,10 +1,24 @@
 <?php
 
-
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class CurrentDateController
+ * @package App\Controller
+ */
 class CurrentDateController
 {
+    /**
+     * @Route(path="/index")
+     * @return Response
+     */
+    public function main() : Response
+    {
+        $currentDate = new \DateTime();
 
+        return new Response('<html><head></head><body>' . $currentDate->format(DATE_ATOM) . '</body></html>');
+    }
 }
